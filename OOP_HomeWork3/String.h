88d734597e179627e94bf6a3ed525a0943ef701a
Vyuga Snow <Wolf-snow00@mail.ru>
2022-12-14 18:33:16 +0300

    cf

--- OOP_HomeWork3/String.h
+++ OOP_HomeWork3/String.h
@@ -5,35 +5,41 @@ using namespace std;
 
 class String
 {
-	
-
 private:
 	int count = 0;
-	int size = 0;
-	char* str = new char[size];
+	char* str;
 public:
+	//Move constructor
+	String(String&& p) noexcept : 
+		str{ nullptr }, count{ NULL }
+	{
+		str = p.str;
+		count = p.count;
+
+		p.str = nullptr;
+		p.count = NULL;
+	}
 	//Default constructor
 	String() {
-		String::size = 80;
-		char* buffDef = new char[80];
+		str = new char[80];
 		cout << "Enter a string:\n";
-		gets_s(buffDef, 81);
-		String::str = buffDef;
+		gets_s(str, 81);
 		count++;
 	}
 	//Constructor with custom size
 	String(int size) : String() {
-		String::size = size;
-		char* buffDef = new char[size];
-		strcpy_s(buffDef, size + 1, str);
-		String::str = buffDef;
+		if (str)
+			delete[]str;
+		str = new char[size];
+		gets_s(str, size+1);
+		count++;
 	}
 	//Custom size string and user's string
 	String(char* str, int size) {
-		String::size = size;
-		char* buff = new char[size];
-		buff = str;
-		String::str = buff;
+		if (str)
+			delete[]str;
+		str = new char[size];
+		gets_s(str, size + 1);
 		count++;
 	}
 	//Destructor (I don't know how true, but I hope so)
